00939f8f9c86cbdfba6bdc82302944de6930670a
Hactazia <hactazia@gmail.com>
2026-05-08 05:08:06 +0200

    feat: enhance ThenHelper with async task handling and improved error management

--- Utils/ThenHelper.cs
+++ Utils/ThenHelper.cs
@@ -1,17 +1,45 @@
 using System;
+using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
 
-namespace Nox.CCK.Utils {
-	public static class ThenHelper {
-		public static async UniTask Then<T>(
-			this UniTask<T>   task,
-			Action<T>         onSuccess = null,
-			Action<Exception> onError   = null
-		) {
-			try {
+namespace Nox.CCK.Utils
+{
+	public static class ThenHelper
+	{
+		public static UniTask Then<T>(
+			this UniTask<T> task,
+			Action<T> onSuccess = null,
+			Action<Exception> onError = null
+		) => task
+			.AsTask()
+			.Then(onSuccess, onError)
+			.AsUniTask();
+
+		public static async Task Then<T>(
+			this Task<T> task,
+			Action<T> onSuccess = null,
+			Action<Exception> onError = null
+		)
+		{
+			if (task.IsCompletedSuccessfully)
+			{
+				onSuccess?.Invoke(task.Result);
+				return;
+			}
+
+			if (task.IsFaulted)
+			{
+				onError?.Invoke(task.Exception);
+				return;
+			}
+
+			try
+			{
 				var result = await task;
 				onSuccess?.Invoke(result);
-			} catch (Exception ex) {
+			}
+			catch (Exception ex)
+			{
 				onError?.Invoke(ex);
 			}
 		}
